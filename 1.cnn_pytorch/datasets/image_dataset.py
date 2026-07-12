49aff8fa5eb71c7d6c421d2be27ea93d94a9a542
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-07-13 00:02:17 +0330

    update : change data set

--- 1.cnn_pytorch/datasets/image_dataset.py
+++ 1.cnn_pytorch/datasets/image_dataset.py
@@ -1,33 +1,34 @@
 from torchvision import datasets
 from torchvision import transforms
+from torch.utils.data import random_split
 
 
-def get_train_dataset():
+def get_datasets():
 
     transform = transforms.Compose([
-        transforms.ToTensor(),
+        transforms.ToTensor()
     ])
 
-    dataset = datasets.CIFAR10(
+    train_dataset = datasets.CIFAR10(
         root="data",
         train=True,
         download=True,
         transform=transform
     )
 
-    return dataset
-
-def get_test_dataset():
-
-    transform = transforms.Compose([
-        transforms.ToTensor()
-    ])
-
-    dataset = datasets.CIFAR10(
+    test_dataset = datasets.CIFAR10(
         root="data",
         train=False,
         download=True,
         transform=transform
     )
 
-    return dataset
+    train_size = int(0.8 * len(train_dataset))
+    valid_size = len(train_dataset) - train_size
+
+    train_dataset, valid_dataset = random_split(
+        train_dataset,
+        [train_size, valid_size]
+    )
+
+    return train_dataset, valid_dataset, test_dataset
