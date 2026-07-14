1165969e38ad1d96c47323d5ee78678cf5785d95
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-07-14 22:31:57 +0330

    Update test.py

--- 2.vision-classification/test.py
+++ 2.vision-classification/test.py
@@ -1,3 +1,13 @@
 from datasets.test_dataset import test_dataset
+import torch
+from models.simple_cnn import SimpleCNN
 
 test_dataset()
+
+x = torch.randn(4, 3, 150, 150)
+
+model = SimpleCNN()
+
+output = model(x)
+
+print(output)
