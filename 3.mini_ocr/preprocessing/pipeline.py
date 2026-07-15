40a2011e5ded86f131e962f2fc2c6c17ac23c4f0
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-07-16 01:11:48 +0330

    update : use blur pipeline

--- 3.mini_ocr/preprocessing/pipeline.py
+++ 3.mini_ocr/preprocessing/pipeline.py
@@ -1,11 +1,12 @@
 from .grayscale import to_grayscale
 from .threshold import otsu_threshold
-
+from .blur import median_blur
 
 class PreprocessingPipeline:
 
     def process(self, image):
         image = to_grayscale(image)
+        image = median_blur(image)
         image = otsu_threshold(image)
 
         return image
