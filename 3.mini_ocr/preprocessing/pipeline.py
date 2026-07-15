fede01613147dcf021ba57965ff5f9510a883824
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-07-16 01:06:06 +0330

    update : implement thresholds

--- 3.mini_ocr/preprocessing/pipeline.py
+++ 3.mini_ocr/preprocessing/pipeline.py
@@ -1,7 +1,11 @@
 from .grayscale import to_grayscale
+from .threshold import otsu_threshold
+
 
 class PreprocessingPipeline:
 
     def process(self, image):
         image = to_grayscale(image)
+        image = otsu_threshold(image)
+
         return image
