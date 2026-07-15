b44bd8eb3bb307fc8ac4fd133135a9fdb5afeeb3
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-07-16 00:35:44 +0330

    create : playground for test

---
+++ 3.mini_ocr/playground.py
@@ -0,0 +1,24 @@
+from utils.image import load_image,save_image,show_image
+from preprocessing.grayscale import cvtGrayScale
+
+path = "samples/img-test.png"
+
+image = load_image(path)
+print(image.shape)
+print(image.dtype)
+
+#show_image(image)
+
+# for i in range(100):
+#     image[i, i] = [255, 0, 0]
+#     for j in range(i):
+#         image[i, j] = [255, 0, 0]
+#
+# save_image("samples/img-test-2.jpg",image=image)
+
+print(image.min())
+print(image.max())
+print(image.mean())
+
+gray_scale = cvtGrayScale(image)
+save_image("samples/img-test-gray_scale.jpg",image=gray_scale)
