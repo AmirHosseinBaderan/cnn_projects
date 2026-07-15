995df8ce54097e9e5bf09491736f3ca38650a199
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-07-16 01:46:57 +0330

    Update playground.py

--- 3.mini_ocr/playground.py
+++ 3.mini_ocr/playground.py
@@ -1,16 +1,32 @@
 import cv2
 import numpy as np
-from utils.image import load_image,save_image,show_image
-from preprocessing.grayscale import to_grayscale
+from utils.image import load_image, show_image
 from preprocessing.pipeline import PreprocessingPipeline
+from detection.contour_detector import ContourDetector
+
+contour_detector = ContourDetector()
+
+
+def bounding_box():
+    output = result.copy()
+    for contour in contours:
+        x, y, w, h = cv2.boundingRect(contour)
+
+        cv2.rectangle(
+            output,
+            (x, y),
+            (x + w, y + h),
+            (255, 255, 255),
+            1
+        )
+
+    show_image(output)
+
 
 path = "samples/noise-image.png"
 
 image = load_image(path)
-print(image.shape)
-print(image.dtype)
-
-#show_image(image)
+# show_image(image)
 
 # for i in range(100):
 #     image[i, i] = [255, 0, 0]
@@ -19,14 +35,14 @@ print(image.dtype)
 #
 # save_image("samples/img-test-2.jpg",image=image)
 
-print(image.min())
-print(image.max())
-print(image.mean())
-
+# print(image.min())
+# print(image.max())
+# print(image.mean())
 
 pipeline = PreprocessingPipeline()
 result = pipeline.process(image)
 
-save_image("samples/noise-image-2.png",image=result)
+contours = contour_detector.detect(result)
+print(f'Contours detected: {len(contours)}')
 
-show_image(result)
+bounding_box()
