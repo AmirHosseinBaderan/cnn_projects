117be46598a30e2c41b8a19dad38409375f8dd3d
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-07-16 23:54:41 +0330

    update: src/similarity.py

---
+++ 3.face_recognition/src/similarity.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn.functional as F
+
+
+def cosine_similarity(
+        embedding1,
+        embedding2
+):
+    return F.cosine_similarity(
+        embedding1,
+        embedding2,
+    ).item()
