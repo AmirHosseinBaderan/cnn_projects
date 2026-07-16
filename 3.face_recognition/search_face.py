17555cd73e68c4896715680becb377f952af31d6
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-07-16 23:39:53 +0330

    update: search_face.py

---
+++ 3.face_recognition/search_face.py
@@ -0,0 +1,74 @@
+import torch
+
+from src.model import FaceEmbeddingNet
+from src.config import Config
+from src.checkpoint import load_checkpoint
+
+from src.database import FaceDatabase
+from src.search import FaceSearch
+from src.evaluator import get_embedding
+from src.transforms import FaceTransform
+
+import cv2
+
+
+
+def load_image(path):
+
+    image = cv2.imread(path)
+
+    image = cv2.cvtColor(
+        image,
+        cv2.COLOR_BGR2RGB
+    )
+
+    transform = FaceTransform()
+
+    return transform(image)
+
+
+
+model = FaceEmbeddingNet().to(
+    Config.DEVICE
+)
+
+
+load_checkpoint(
+    Config.CHECKPOINT_PATH,
+    model
+)
+
+
+
+database = FaceDatabase()
+
+
+embeddings, names = database.load()
+
+
+
+search_engine = FaceSearch(
+    embeddings,
+    names
+)
+
+
+
+image = load_image(
+    "./data/test/unknown.jpg"
+)
+
+
+
+query_embedding = get_embedding(
+    model,
+    image,
+    Config.DEVICE
+)
+
+result = search_engine.search(
+    query_embedding
+)
+
+
+print(result)
