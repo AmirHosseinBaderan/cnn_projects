8d37708c67564ab47cab99f070a41f71a6c4ab5f
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-07-21 12:29:31 +0330

    update : test multi line

--- 4.ocr/predict.py
+++ 4.ocr/predict.py
@@ -1,10 +1,15 @@
+import cv2
+
 from inference.engine import OCREngine
+
 
 ocr = OCREngine(
     checkpoint_path="checkpoints/best.pt",
     vocabulary_file="resources/vocab.json",
 )
 
-text = ocr.predict("data/test/test_5.jpg")
+image = cv2.imread("data/test/test_6.png")
+
+text = ocr.predict(image)
 
 print(text)
