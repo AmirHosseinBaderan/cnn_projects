4ba0f8d5acf19d72b7f0cdde4193aabb288d534a
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-07-27 00:38:48 +0330

    Update main.py

--- 5.alpr/main.py
+++ 5.alpr/main.py
@@ -1,16 +1,40 @@
 from dataset.detector_dataset import DetectorDataset
 from dataset.parsers import IRLPRXMLParser
 
+from preprocessing.transforms import (
+    Compose,
+    Resize,
+    ToTensor,
+    Normalize
+)
+
 
 dataset = DetectorDataset(
     root="data/car_images/train",
     parser=IRLPRXMLParser(),
+    transform=Compose(
+        [
+            Resize(
+                width=640,
+                height=640,
+            ),
+            ToTensor(),
+            Normalize()
+        ]
+    ),
 )
 
-print(len(dataset))
-
 sample = dataset[0]
-print(sample)
+
+print(sample["image"].shape)
+
+print(sample["annotation"].image.width)
 
 plate = sample["annotation"].first("کل ناحیه پلاک")
-print(plate)
+
+print(plate.bbox)
+
+print(sample["image"].shape)
+print(sample["image"].dtype)
+print(sample["image"].min())
+print(sample["image"].max())
