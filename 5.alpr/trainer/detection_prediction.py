2b2d6ab25a25a6a1b67f82b37f1b0dc8d161b109
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-07-27 18:10:27 +0330

    update : add device change func

--- 5.alpr/trainer/detection_prediction.py
+++ 5.alpr/trainer/detection_prediction.py
@@ -8,3 +8,10 @@ class DetectionPrediction:
     boxes: torch.Tensor
     objectness: torch.Tensor
     classes: torch.Tensor
+    
+    def to(self, device):
+            return DetectionPrediction(
+                boxes=self.boxes.to(device),
+                objectness=self.objectness.to(device),
+                classes=self.classes.to(device),
+            )
