fc9c3270c8f8a548d1e87763a91e54f7353091cf
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-07-27 16:03:51 +0330

    update : change bacbone

--- 5.alpr/models/modules/backbone.py
+++ 5.alpr/models/modules/backbone.py
@@ -9,16 +9,28 @@ class Backbone(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.layers = nn.Sequential(
-            ConvBlock(3, 32, stride=2),
-            ConvBlock(32, 64, stride=2),
-            ConvBlock(64, 128, stride=2),
-            ConvBlock(128, 256, stride=2),
-            ConvBlock(256, 512, stride=2),
-        )
+        self.stage1 = ConvBlock(3, 32, stride=2)
+
+        self.stage2 = ConvBlock(32, 64, stride=2)
+
+        self.stage3 = ConvBlock(64, 128, stride=2)
+
+        self.stage4 = ConvBlock(128, 256, stride=2)
+
+        self.stage5 = ConvBlock(256, 512, stride=2)
 
     def forward(
         self,
         x: torch.Tensor,
-    ) -> torch.Tensor:
-        return self.layers(x)
+    ):
+
+        x = self.stage1(x)
+        x = self.stage2(x)
+
+        p3 = self.stage3(x)
+
+        p4 = self.stage4(p3)
+
+        p5 = self.stage5(p4)
+
+        return p3, p4, p5
