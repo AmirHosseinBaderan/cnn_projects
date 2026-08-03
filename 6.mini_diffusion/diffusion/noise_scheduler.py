9931f3528e8a84a5f902ae56ada06115c6b5b75f
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-08-03 12:36:46 +0330

    update : use beta scheduler

--- 6.mini_diffusion/diffusion/noise_scheduler.py
+++ 6.mini_diffusion/diffusion/noise_scheduler.py
@@ -1,18 +1,50 @@
 import torch
 
+from diffusion.beta_scheduler import linear_beta_schedule
+
 
 class NoiseScheduler:
 
     def __init__(
         self,
         num_timesteps: int = 1000,
-        beta_start: float = 1e-4,
-        beta_end: float = 2e-2,
+        device="cpu",
     ):
         self.num_timesteps = num_timesteps
+        self.device = device
 
-        self.betas = torch.linspace(
-            beta_start,
-            beta_end,
-            num_timesteps,
+        self.betas = linear_beta_schedule(
+            num_timesteps
+        ).to(device)
+
+        self.alphas = 1 - self.betas
+
+        self.alpha_bars = torch.cumprod(
+            self.alphas,
+            dim=0,
         )
+
+
+    def add_noise(
+        self,
+        images,
+        timesteps,
+    ):
+        noise = torch.randn_like(images)
+
+        alpha_bar = self.alpha_bars[timesteps]
+
+        alpha_bar = alpha_bar.view(
+            -1,
+            1,
+            1,
+            1,
+        )
+
+        noisy_images = (
+            torch.sqrt(alpha_bar) * images
+            +
+            torch.sqrt(1 - alpha_bar) * noise
+        )
+
+        return noisy_images, noise
