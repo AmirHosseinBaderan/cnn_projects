7a8472d17fa4937db57e1498a7436421cb687737
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-08-03 14:30:08 +0330

    Update predict.py

--- 6.mini_diffusion/predict.py
+++ 6.mini_diffusion/predict.py
@@ -8,25 +8,32 @@ predictor = DiffusionPredictor(
     checkpoint_path="checkpoints/best.pt"
 )
 
+while True:
+    label = input("Give label : ")
+    if label.lower() == "exit":
+        print("---- exit -----")
+        break
+    
+    try:
+        int_label = int(label)
+    except:
+        print("just enter int or exit")
+        continue
+    
+    image = predictor.generate(
+        label=int_label
+    )
 
-image = predictor.generate(
-    label=7
-)
-
-image = image.squeeze().cpu()
+    image = image.squeeze().cpu()
 
 
-plt.figure(figsize=(3,3))
-
-plt.imshow(
-    image,
-    cmap="gray"
-)
-
-plt.title(
-    "Generated Digit: 7"
-)
-
-plt.axis("off")
-
-plt.show()
+    plt.figure(figsize=(3,3))
+    plt.imshow(
+        image,
+        cmap="gray"
+    )
+    plt.title(
+        f"Generated Digit: {int_label}"
+    )
+    plt.axis("off")
+    plt.show()
